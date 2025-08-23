058f9157ac6120fe02592ea8b848fe103e20d2ba
raghunath <raghunath.e@gmail.com>
2025-08-23 14:41:04 -0700

    Add dynamic /greet/<name> endpoint with test cases

--- app.py
+++ app.py
@@ -9,5 +9,11 @@ def hello_world():
     ip_address = socket.gethostbyname(hostname)
     return f"Hello World! Server IP: {ip_address}"
 
+@app.route('/greet/<name>')
+def greet_person(name):
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return f"Hello, {name}! Server IP: {ip_address}"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
