9a0f88fafc6075952c37252abbac3291c273fcc5
raghunath <raghunath.e@gmail.com>
2025-08-23 12:33:24 -0700

    Initial commit: Flask Hello World app with tests

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return f"Hello World! Server IP: {ip_address}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
